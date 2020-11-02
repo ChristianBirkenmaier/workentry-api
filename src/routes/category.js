ab46c7c5c80cdc7d3bfef194be5e59d7c10621b5
Birkenmaier Christian <CBirkenmaier@noerpel-gruppe.de>
2020-11-02 10:44:22 +0100

    implemented crud for projects and categories

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,19 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const { Category } = require("../models");
 
 // define the home page route
-router.get("/", (req, res) => {
-    res.send("Category get all");
+router.get("/", async (req, res) => {
+    try {
+        // res.send("Category get all");
+        const categories = await Category.find();
+        res.json({ ok: true, data: categories });
+    } catch (err) {
+        console.error(err);
+        res.json({ ok: false, error: err });
+    }
     // Alle Zeiteinträge zurückgeben
 });
 // define the about route
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     const { id } = req.params;
+    try {
+        // res.send("Category get all");
+        const category = await Category.find(id);
+        res.json({ ok: true, data: category });
+    } catch (err) {
+        console.error(err);
+        res.json({ ok: false, error: err });
+    }
     // Zeiteintrag mit bestimmter id zurückgeben
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     // Neuen Zeiteintrag erstellen
+    try {
+        const { categoryName } = req.body;
+        const newCategory = await Category.create({
+            categoryName,
+        });
+        res.json({
+            ok: true,
+            data: newCategory,
+        });
+    } catch (err) {
+        console.error("Error: ", err);
+        res.json({ ok: false, error: err });
+    }
 });
 
 router.put("/:id", (req, res) => {
--- src/routes/project.js
+++ src/routes/project.js
@@ -1,19 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const { Project } = require("../models");
 
 // define the home page route
-router.get("/", (req, res) => {
-    res.send("Project get all");
+router.get("/", async (req, res) => {
+    try {
+        // res.send("Category get all");
+        const projects = await Project.find();
+        res.json({ ok: true, data: projects });
+    } catch (err) {
+        console.error(err);
+        res.json({ ok: false, error: err });
+    }
     // Alle Zeiteinträge zurückgeben
 });
 // define the about route
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     const { id } = req.params;
+    try {
+        // res.send("Category get all");
+        const project = await Project.find(id);
+        res.json({ ok: true, data: project });
+    } catch (err) {
+        console.error(err);
+        res.json({ ok: false, error: err });
+    }
     // Zeiteintrag mit bestimmter id zurückgeben
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     // Neuen Zeiteintrag erstellen
+    try {
+        const { projectName } = req.body;
+        const newProject = await Project.create({
+            projectName,
+        });
+        res.json({
+            ok: true,
+            data: newProject,
+        });
+    } catch (err) {
+        console.error("Error: ", err);
+        res.json({ ok: false, error: err });
+    }
 });
 
 router.put("/:id", (req, res) => {
