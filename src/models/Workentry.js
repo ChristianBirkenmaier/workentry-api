51ff83dd59f5f64dcacea934ee9f888a289b9e18
Birkenmaier Christian <CBirkenmaier@noerpel-gruppe.de>
2020-11-02 10:55:26 +0100

    bug: projectname -> projectName in workentry model

--- src/models/Workentry.js
+++ src/models/Workentry.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const workentrySchema = new Schema({
     categoryName: { type: Schema.Types.ObjectId, ref: "Category" },
-    projectname: { type: Schema.Types.ObjectId, ref: "Project" },
+    projectName: { type: Schema.Types.ObjectId, ref: "Project" },
     fromDate: String,
     untilDate: String,
     optionalText: String,
